7bfd8e546f75778a40ffa6c095de7dbaeac7a2d8
Labkom99 <71483435+labkom99@users.noreply.github.com>
2022-01-09 11:21:08 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,48 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Temon!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my SITE Temon!" />
+        <a href="https://www.temon.eu.org/2022/01/3-principles-that-parents-should-have.html">parents</a>
+        <a href="https://www.temon.eu.org/2022/01/when-pregnant-women-eat-chili-amniotic.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-gourd-be-eaten-by-pregnant-women.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-mung-bean-sprouts-rich-be-eaten-by.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-taro-be-eaten-by-pregnant-women.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-garlic-sprouts.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-pakchoi.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-carob.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-asparagus.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-fencao.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-cactus.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-pregnant-women-eat-purple-potatoes.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/can-corn-be-eaten-by-pregnant-women.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/what-should-i-do-if-child-throws-toys.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/what-sleeping-position-should-be-best.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/does-baby-wear-socks-to-sleep.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/baby-exercises-should-be-protected-to.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/what-does-child-want-from-parents.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2022/01/girls-have-these-three-characteristics.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2021/09/how-to-raise-self-disciplined-and.html">pregnant</a>
+        <a href="https://www.temon.eu.org/2021/09/an-important-ability-that-should-be.html">Temon</a>
+        <a href="https://www.temon.eu.org/2021/09/3-personality-defects-of-children-if.html">Temon</a>
+        <a href="https://www.temon.eu.org/2021/09/impact-of-buying-lots-of-toys-for.html">Temon</a>
+        <a href="https://www.temon.eu.org/2021/09/why-do-children-lie-parents-must-be.html">Temon</a>
+        <a href="https://www.temon.eu.org/2021/09/how-to-educate-your-children.html">Temon</a>
+        <a href="https://www.temon.eu.org/">Temon</a>
+        <a href="https://www.temon.eu.org/p/about-us.html">Temon</a>
+        <a href="https://www.temon.eu.org/p/contact-us.html">Temon</a>
+        <a href="https://www.temon.eu.org/p/privacy-and-policy.html">Temon</a>
+        <a href="https://www.temon.eu.org/p/disclaimer-for-temon.html">Temon</a>
+        <a href="https://www.temon.eu.org/p/terms-and-conditions.html">Temon</a>
+        <a href="https://www.temon.eu.org/p/sitemap.html">Temon</a>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        
       </main>
 
       <Footer />
